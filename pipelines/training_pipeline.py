a397896ebd923b923c8b2328c6d49c61232fd5bd
anujvaghani0 <anujvaghani0@gmail.com>
2023-09-10 14:55:42 +0530

    evaluation,model_train,model_dev added into main pipeline

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -1,13 +1,25 @@
-from zenml import pipeline
-from steps.ingest_data import ingest_df
-from steps.clean_data import clean_df
-from steps.evaluation import evaluation_model
+from steps.ingest_data import ingest_data
+from steps.clean_data import clean_data
 from steps.model_train import model_train
+from steps.evaluation import evaluation
+from zenml import pipeline
 
-@pipeline
-def train_pipeline(data_path: str):
-    df=ingest_df(data_path)
-    clean_df(df)
-    model_train(df)
-    evaluation_model(df)
 
+
+@pipeline(enable_cache=True)
+def train_pipeline(data_path:str):
+    """
+    Args:
+        ingest_data: DataClass
+        clean_data: DataClass
+        model_train: DataClass
+        evaluation: DataClass
+    Returns:
+        mse: float
+        rmse: float
+    """
+    df = ingest_data()
+    x_train, x_test, y_train, y_test = clean_data(df)
+    model = model_train(x_train, x_test, y_train, y_test)
+    r2_score, rmse = evaluation(model, x_test, y_test)
+    return r2_score, rmse
