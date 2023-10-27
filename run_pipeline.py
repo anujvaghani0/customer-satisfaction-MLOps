cdb578920ce0c7ec94819a0840a389cd6ec0b3ee
anujvaghani0 <anujvaghani0@gmail.com>
2023-10-27 18:47:37 +0530

    added

--- run_pipeline.py
+++ run_pipeline.py
@@ -5,3 +5,4 @@ if __name__== "__main__":
     # Run the pipeline
     train_pipeline(data_path="/Users/anujvaghani0/Developer/customer-satisfaction-MLOps/data/olist_customers_dataset.csv")
     logging.info("Pipeline run completed")
+#testing
